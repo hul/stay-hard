16fe8bf4b243f0093ee24351d3766cc04cfd2688
hul <lhulanicki@gmail.com>
2025-05-03 14:21:22 +0200

    center logo

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,21 @@
 </script>
 
 <template>
-  <div>
+  <div class="container">
     <img src="/stay-hard-512x512.png" alt="Stay Hard logo" />
   </div>
 </template>
+
+<style scoped>
+.container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+</style>
